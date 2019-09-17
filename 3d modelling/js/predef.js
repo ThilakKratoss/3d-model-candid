288f4043583bb39bd1951a5179fd7c08785c2cb1
thilak <thilakal9299@gmail.com>
2019-09-17 14:44:30 +0530

    first commit

---
+++ 3d modelling/js/predef.js
@@ -0,0 +1,32 @@
+var replacements={
+    'spear':'sphere',
+    'cube':'box',
+    'cuboid':'box',
+}
+
+var colors={
+    red:0xe51c23,
+    pink:0xe91e63,
+    purple:0x9c27b0,
+    indigo:0x3f51b5,
+    blue:0x2196f3,
+    green:0x4caf50,
+    yellow:0xffeb3b,
+    orange:0xff9800,
+    brown:0x795548,
+    black:0x000000
+}
+
+var shapes=[
+    'sphere',
+    'box',
+    'cylinder',
+]
+
+var size = { 
+    'large': { x: 5, y: 5, z: 5 }, 
+    'small': { x: 1, y: 1, z: 1 }, 
+    'tiny': { x: 0.5, y: 0.5, z: 0.5 }, 
+    'tall': { x: 1, y: 4, z: 1 }, 
+    'flat': { x: 5, y: 0.5, z: 5 }
+}
